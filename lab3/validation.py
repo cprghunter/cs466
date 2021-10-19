bc6d2982b8964a308e91bc408bf337095b41ced7
Ryan Hunter <rghunter@calpoly.edu>
2021-10-19 16:16:28 -0700

    resolve merge conflict

--- lab3/validation.py
+++ lab3/validation.py
@@ -1,34 +1,64 @@
 import Classifier as c
-import InduceC45 as c45
+import Induce45 as c45
 import sys
+import pandas
 
 
 def generate_data_subsets(df, restr_file, k):
     data_subsets = []
-    data_len = df.shape[0]
-    if k != -1:
-        for i in range(k):
-            subset = df.sample(n=max(df.shape[0],int(data_len/k)))
-            data_subsets.append(subset)
-            df.drop(subset.index)
-            c45(df, restr_file)
-            matrix, stats = Classifier(subset.to_csv(), 'decision_tree.json')
-            print(matrix, stats)
-            
-    else:
-        for i in range(data_len):
-            continue
+    data_len = df.shap[0]
+    for i in range(k):
+        subset = df.sample(n=max(df.shape[0],int(data_len/k)))
+        data_subsets.append(subset)
+        df.drop(subset.index)
+    return data_subsets
 
-def validate(sets, k):
-    for i in range(0, k):
+def get_training_dataset(i, data_subsets):
+    df = pandas.DataFrame()
+    for j  in range(len(data_subsets)):
+        if j != i:
+            df.append(data_subsets[j])
+    return df
 
+def kfold(data_subsets, attributes, threshold, class_attr, 
+          class_labels, attr_domain_dict):
+
+    for i in range(len(data_subsets)):
+        test = data_subset[i]
+        training = get_training_dataset(i, data_subsets)
+
+        # produces json
+        c45.c45_produce_json(training, attributes, threshold, class_attr,
+                            class_labels, attr_domain_dict)
+
+        # classify and generate statistics
+
+def all_but_one(df, attributes, threshold, class_attr, 
+          class_labels, attr_domain_dict): 
+
+    for i in range(len(df)):
+        test = df.loc[i]
+        df.drop(test)
+
+        # produces json
+        c45.c45_produce_json(df, attributes, threshold, class_attr,
+                            class_labels, attr_domain_dict)
+
+        # classify and generate statistics
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
+    if sys.argc == 4:
         training_file = sys.argv[1]
         restr_file = sys.argv[2]
         k = int(sys.argv[3])
     else:
         training_file = sys.argv[1]
         k = int(sys.argv[2])
+
+    df = pandas.read_csv(training_file)
+    class_attr = data_df.iloc[1][0]
+    data_df = data_df.drop(labels=[0, 1])
+    class_attr_values = data_df[class_attr].unique()
+    attributes = [attr for attr in df.columns if not attr == class_attr]
+    attr_domain_dict = c45.build_attr_domain_dict(data_df, attributes)
 
