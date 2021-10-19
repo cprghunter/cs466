7d9fd9ad7e5ae1a1ef05f8149ad80c9bff18a8f5
Ryan Hunter <rghunter@calpoly.edu>
2021-10-19 13:33:18 -0700

    bit more of validation

--- lab3/validation.py
+++ lab3/validation.py
@@ -1,8 +1,19 @@
 import Classifier as c
 import sys
 
-def all_but_one(training_set):
-    return 0
+
+def generate_data_subsets(df, k):
+    data_subsets = []
+    data_len = df.shap[0]
+    if k != -1:
+        for i in range(k):
+            subset = df.sample(n=max(df.shape[0],int(data_len/k)))
+            data_subsets.append(subset)
+            df.drop(subset.index)
+    else:
+        for i in range(data_len):
+            continue
+
 
 if __name__ == "__main__":
     if sys.argc == 4:
@@ -12,5 +23,4 @@ if __name__ == "__main__":
     else:
         training_file = sys.argv[1]
         k = int(sys.argv[3])
-    if k == -1:
-        all_but_one(training_file)
+
