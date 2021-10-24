9db6f929f3d21838dcf1dc9f02c0ebac2ed20929
Taylor Bedrosian <taylorbedrosian@MacBook-Pro.local>
2021-10-24 10:22:48 -0700

    laptop-desktop sync push

--- lab3/KNN.py
+++ lab3/KNN.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
+import sys
 
-def knn():
+def knn(dataset, k, test_data):
+    dataset = pd.read_csv(dataset)
+    test_data = pd.read_csv(test_data)
+    print(dataset, test_data)
     return
 
 if __name__ == '__main__':
+    dataset = sys.argv[1]
+    k = int(sys.argv[2])
+    test_data = sys.argv[3]
     knn(dataset, k, test_data)
